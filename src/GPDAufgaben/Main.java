a1ca7bdd29cc41fdec9ae4d1e2a342568437aae7
AlexanderPaul1983 <alexanderpaul@gmx.de>
2023-05-23 22:36:18 +0200

    instanzen Typs Fahrzeug erstellt

--- src/GPDAufgaben/Main.java
+++ src/GPDAufgaben/Main.java
@@ -3,12 +3,10 @@ package GPDAufgaben;
 public class Main {
     public static void main(String[] args) {
 
-        Bibliothek bibliothek = new Bibliothek("Stadtsbibliothek");
-        Buch buch1 = new Buch("Harry Potter","Stein", "httr12345");
-        Buch buch2 = new Buch("Der Kleine Prinz","Josef","djhf834726");
-        bibliothek.addBook(buch1);
-        bibliothek.addBook(buch2);
-        bibliothek.showAllbooks();
+        Fahrzeug audi = new Fahrzeug(15,"Audi");
+        Fahrzeug bmw = new Fahrzeug(16,"BMW");
+        audi.beschleunigen(120);
+        System.out.println(audi.getGeschwindichkeit());
 
 
 
