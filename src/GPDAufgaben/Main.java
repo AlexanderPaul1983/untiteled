d32bd8a0d0a58cbf9fb193d9b38a5bdd27040bff
AlexanderPaul1983 <alexanderpaul@gmx.de>
2023-05-31 21:46:42 +0200

    The Project a LinkedList to create was started

--- src/GPDAufgaben/Main.java
+++ src/GPDAufgaben/Main.java
@@ -5,8 +5,11 @@ public class Main {
 
         Fahrzeug audi = new Fahrzeug(15,"Audi");
         Fahrzeug bmw = new Fahrzeug(16,"BMW");
+        Fahrzeug WV = new Fahrzeug(45,"WV");
         audi.beschleunigen(120);
         System.out.println(audi.getGeschwindichkeit());
+        System.out.println(Fahrzeug.getAnzahlFahrzeuge());
+        
 
 
 
